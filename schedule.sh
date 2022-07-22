ec56a8d7288a2008ccb22321880ad5a113de67f1
Waqas Mustafa <malik.waqas.09@hotmail.com>
2022-07-22 15:10:51 +0500

    supervisor conf reverted

--- schedule.sh
+++ schedule.sh
@@ -1,3 +1,15 @@
+
+[program:schedule]
+command = /bin/bash -c "/var/www/html/schedule.sh"
+stdout_logfile = /dev/fd/1
+stdout_logfile_maxbytes=0
+stderr_logfile = /dev/fd/2
+stderr_logfile_maxbytes=0
+user = root
+autostart = true
+autorestart = true
+priority = 20
+
 while [ true ]
 do
  php /var/www/html/artisan schedule:run --verbose --no-interaction &
